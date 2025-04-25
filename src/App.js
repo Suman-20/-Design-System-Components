3c4e4eb289150e200b3f1e713fcacd49d99812d0
Raulsuman8689 <sumanraul74@gmail.com>
2025-04-25 12:50:22 +0530

    edit

--- src/App.js
+++
@@ -1,6 +0,0 @@
-import { jsx as _jsx, jsxs as _jsxs } from "react/jsx-runtime";
-import Heading from './components/Typography/Heading';
-function App() {
-    return (_jsxs("div", { children: [_jsx(Heading, { level: 1, className: "text-red-300", children: "H1 Heading" }), _jsx(Heading, { level: 3, className: "text-red-500", children: "H3 Tailwind Heading" })] }));
-}
-export default App;
--- src/utils/cn.js
+++
@@ -1,6 +0,0 @@
-// src/utils/cn.ts
-import { clsx } from "clsx";
-import { twMerge } from "tailwind-merge";
-export function cn(...inputs) {
-    return twMerge(clsx(inputs));
-}
